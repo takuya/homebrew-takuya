a8281935396ad85391aa83fa864355806be905b9
takuya <takuya.1st+nospam@gmail.com>
2015-12-12 15:25:50 +0900

    実験

---
+++ launchctl-completion.rb
@@ -0,0 +1,20 @@
+class BundlerCompletion < Formula
+  desc "Bash completion for Launchctl"
+  homepage ""
+  url "https://github.com/bobthecow/launchctl-completion",
+    :revision => "f83bfbaa569d16f4951bae16aebff64fa498dfc7"
+  version "2"
+
+  head "https://github.com/bobthecow/launchctl-completion.git"
+
+  bottle :unneeded
+
+  def install
+    bash_completion.install "completion-launchctl" => "launchctl"
+  end
+
+  test do
+    assert_match "-F __bundle",
+      shell_output("source #{bash_completion}/bundler && complete -p bundle")
+  end
+end
