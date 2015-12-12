a4dcd77b625c82bd88883248026a063a2753ec57
takuya <takuya.1st+nospam@gmail.com>
2015-12-13 00:49:01 +0900

    実験

--- launchctl-completion.rb
+++ launchctl-completion.rb
@@ -1,10 +1,13 @@
-class LaunchctlCompletion < Formula
-  desc "Bash completion for Launchctl"
-  homepage ""
-  url "https://github.com/bobthecow/launchctl-completion",
-    :revision => "f83bfbaa569d16f4951bae16aebff64fa498dfc7"
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                http://www.rubydoc.info/github/Homebrew/homebrew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
+class LaunchctlCompletion < Formula
+  desc "Bash completion for launchctl"
+  homepage "https://github.com/bobthecow/launchctl-completion"
   head "https://github.com/bobthecow/launchctl-completion.git"
+
+  depends_on "bash-completion" # if your formula requires any X11/XQuartz components
 
   bottle :unneeded
 
@@ -14,6 +17,6 @@ class LaunchctlCompletion < Formula
 
   test do
     assert_match "-F __bundle",
-      shell_output("source #{bash_completion}/bundler && complete -p bundle")
+      shell_output("source #{bash_completion}/bundler && complete -p launchctl")
   end
 end
